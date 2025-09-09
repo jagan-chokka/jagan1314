812b1aa11847b88b6d305f9d7a9072dabe997550
Chokka Jagan <chokkajagan111@gmail.com>
2025-09-09 16:10:43 +0530

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Day8 {
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(numbers));
+
+        bubbleSort(numbers);
+
+        System.out.println("Sorted Array:   " + Arrays.toString(numbers));
+    }
+}
