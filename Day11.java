8ed83be50a1cfb6783dd166c8a36830e452bee39
Chokka Jagan <chokkajagan111@gmail.com>
2025-09-11 19:06:15 +0530

    Add files via upload

---
+++ Day11.java
@@ -0,0 +1,83 @@
+import java.util.Arrays;
+
+public class Day11 {
+
+    // Merges two subarrays of arr[].
+    // First subarray is arr[left..mid]
+    // Second subarray is arr[mid+1..right]
+    void merge(int[] arr, int left, int mid, int right) {
+        // Find sizes of two subarrays to be merged
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Create temporary arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        // --- Merge the temp arrays ---
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[left..right] using merge()
+    void sort(int[] arr, int left, int right) {
+        if (left < right) {
+            // Find the middle point
+            int mid = left + (right - left) / 2;
+
+            // Sort first and second halves
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {12, 11, 13, 5, 6, 7};
+        
+        System.out.println("Original Array: " + Arrays.toString(numbers));
+
+        Day11 ob = new Day11();
+        ob.sort(numbers, 0, numbers.length - 1);
+
+        System.out.println("Sorted Array:   " + Arrays.toString(numbers));
+    }
+}
