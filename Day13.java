c4f560df010eae3bd5b86c0a4ea72873ce31ff88
Chokka Jagan <chokkajagan111@gmail.com>
2025-09-13 20:51:43 +0530

    Add files via upload

---
+++ Day13.java
@@ -0,0 +1,37 @@
+public class Day13 {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    Node head;
+
+    public void printList() {
+        Node currentNode = head;
+        System.out.print("LinkedList: ");
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " -> ");
+            currentNode = currentNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Day13 linkedList = new Day13();
+
+        linkedList.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        linkedList.head.next = second;
+        second.next = third;
+
+        linkedList.printList();
+    }
+}
